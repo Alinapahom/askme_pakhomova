1942e564b05a1a2e4abb27f3b02ebd295093203d
Алина Пахомова <alinapahomova@MacBook-Air-Alina.local>
2022-12-21 18:37:47 +0300

    Turn in task4

---
+++ task4/static/js/signup.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//     $("#user-avatar").on('change', function () {
+//         const file_name = $(this).val().replace(/^.*[\\\/]/, '')
+//         $(this).next(".custom-file-label").text(file_name);
+//     })
+// })
+// Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
+(function () {
+  'use strict'
+
+  // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Зацикливайтесь на них и предотвращайте отправку
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
